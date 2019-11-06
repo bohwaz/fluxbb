7eae6caa60b161ef1d6bed557d315468d7e6807e
bohwaz <github.bohwaz@miam.kd2.org>
2019-11-07 00:43:36 +0100

    Fichiers ajoutés pour RL

---
+++ lang/French/register.php
@@ -0,0 +1,40 @@
+<?php
+
+// Language definitions used in register.php
+$lang_register = array(
+
+// Miscellaneous
+'No new regs'			=>	'Ces forums n\'acceptent pas de nouveaux utilisateurs.',
+'Reg cancel redirect'		=>	'Inscription annulée. Redirection&#160;…',
+'Forum rules'			=>	'Conditions d\'utilisation des forums',
+'Rules legend'			=>	'Vous devez accepter les conditions d\'utilisation suivantes pour vous inscrire sur ces forums.',
+'Registration flood'		=>	'Un nouvel utilisateur ayant la même adresse IP que vous a été inscrit dans la dernière heure. Pour éviter les inscriptions multiples, il n\'est pas possible de s\'inscrire à partir de la même IP dans un intervalle inférieur à 1 heure. Désolé pour la gêne occasionnée.',
+'Agree'				=>	'J\'accepte',
+'Cancel'			=>	'Je refuse',
+// Modif RL
+'Register'			=>	'S\'inscrire au forum',
+
+// Form validation stuff (some of these are also used in post.php)
+'Registration errors'		=>	'Erreurs d\'inscription',
+'Registration errors info'	=>	'Les erreurs suivantes doivent être corrigées avant que vous puissiez vous inscrire&#160;:',
+'Username censor'		=>	'Le nom d\'utilisateur que vous avez saisi contient des mots interdits. Veuillez choisir un autre nom d\'utilisateur.',
+'Username dupe 1'		=>	'Il existe déjà une personne inscrite avec ce nom d\'utilisateur.',
+'Username dupe 2'		=>	'Ce nom d\'utilisateur est trop ressemblant. Il doit différer par au moins un caractère alpha-numérique (a-z ou 0-9). Veuillez essayer un autre nom d\'utilisateur.',
+'Email not match'		=>	'Les adresses électroniques ne coïncident pas. Veuillez revenir en arrière et corriger cela.',
+
+// Registration e-mail stuff
+'Reg email'			=>	'Merci de vous être inscrit(e). Votre mot de passe a été envoyé à l\'adresse indiquée. Si vous ne l\'obtenez pas, merci de contacter l\'administrateur&#160;:',
+'Reg complete'			=>	'Inscription acceptée. Identification et redirection&#160;…',
+
+// Register info
+'Desc 1'			=>	'L\'inscription vous permettra d\'accéder à un certain nombre de fonctionnalités autrement indisponibles. Cela inclut la possibilité de modifier et d\'effacer des messages, de créer vos propres signatures et bien davantage. Pour toute question, contactez l\'administrateur.',
+'Desc 2'			=>	'Ci-dessous le formulaire à remplir afin de vous inscrire. Après vous être inscrit(e), vous pourrez consulter votre profil et modifier vos préférences. Les champs ci-dessous ne sont qu\'une toute petite partie de tout ce que vous pouvez changer dans votre profil.',
+'Username legend'		=>	'Veuillez saisir un nom d\'utilisateur comportant entre 2 et 25 caractères.',
+'Pass legend'			=>	'Veuillez saisir et confirmer le mot de passe que vous avez choisi.',
+'Pass info'			=>	'Le mot de passe doit comporter au minimum 6 caractères (sensible à la casse).',
+'Email info'			=>	'Vous devez saisir une adresse électronique valide. Vous pouvez choisir de ne pas divulguer votre adresse aux autres utilisateurs (voir ci-dessous).',
+'Confirm email'			=>	'Confirmez votre adresse électronique',
+//********************* Modif : Antibot
+'Antispam info'            =>    'Afin de nous assurer que ce n\'est pas un robot spammeur qui tente de s\'inscrire : <ul><li>1. trouvez le mot clé de 4 lettres qui répond à cette question : "Qu\'est-ce qui est riche dans ce site ?" (la réponse se trouve dans les règles que vous venez d\'accepter)</li><li>2. <strong>pour que votre inscription soit acceptée faites précéder le <u>premier</u> e-mail ci-dessous du mot clé que vous venez de trouver. Exemple : MotCléVotreEmail@email.fr</strong></li></ul>',
+//********************* Fin Modif
+);
---
+++ uploads/user_header.php
@@ -0,0 +1,27 @@
+<?php
+if ( pun_user_guest ) {
+	require_once('../../redirect.php');
+	exit;
+}
+
+echo '<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>'.($title ? escape($title) . ' - ' : '').$config->title.'</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- BEGIN MODIF META RL -->';
+require '../include/user/header_favicon.php';
+require '../include/user/header_img_aleatoire.php';
+
+echo '
+<link rel="stylesheet" type="text/css" href="' . path_to_forum . 'style/Global/global.css?version=' . current_theme . '" />
+<link rel="stylesheet" type="text/css" href="' . path_to_forum . 'style/' . RLStyle($pun_user['style']) . '.css?version=' . current_theme . '" id="MyCss" />
+' . GetRLStyle() . '
+</head>
+<body id="uploads">
+
+<!-- BEGIN HEADER RL -->';
+require '../include/user/header.php';
+require '../include/user/menuG.php';
